8c5651b14336eff265c8fa67ba8849d8d2d074c2
ratimeriajp <156306448+SalwynnJP@users.noreply.github.com>
2025-11-11 20:38:02 +0900

    Add files via upload

---
+++ hconv/subwin.js
@@ -0,0 +1,5 @@
+window.onerror = null;
+function subwin(pages){
+subw=window.open(pages,'subwin','resizable=yes,scrollbars=yes,location=yes,toolbar=yes,menubar=yes,directories=yes,status=yes,top=30,left=10');
+subw.focus();
+}
